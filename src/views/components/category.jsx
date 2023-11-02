1391d3b4dc10fc94695356b096798f9194348b26
marvel <marvellousogbo0@gmail.com>
2023-11-02 23:50:21 +0100

    fix: mobile responsive

--- src/views/components/category.jsx
+++ src/views/components/category.jsx
@@ -4,17 +4,17 @@ import { categories } from "../../data/data";
 const category = () => {
   console.log(categories);
   return (
-    <div className=" max-w-[1640px] px-4 py-12 m-auto">
+    <div className=" max-w-[1640px] px-4 py-12 mx-auto">
       <h1 className=" text-orange-600 font-bold text-4xl text-center">
         Top Rated Menu Items
       </h1>
-      <div className=" grid grid-cols-2 lg:grid-cols-4 g-6 py-6">
+      <div className=" grid grid-cols-2 md:grid-cols-4 g-6 py-6">
         {categories.map((item) => (
           <div
             key={item.id}
-            className=" bg-gray-200 m-4 flex justify-between items-center p-4"
+            className=" bg-gray-200 m-4 flex justify-between items-center p-4 rounded-lg"
           >
-            <h1 className="font-bold text-xl md:text-2xl">{item.name}</h1>
+            <h1 className="font-bold sm:text-xl">{item.name}</h1>
             <img src={item.image} alt={item.name} className=" w-20" />
           </div>
         ))}
